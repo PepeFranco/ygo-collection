05237bc2917dd9d810949c05659f079563dd5cfb
Pepe <pepe@Pepes-MacBook-Pro.local>
2023-03-24 22:09:26 +1100

    upload working

--- data/uploadCollection.js
+++ data/uploadCollection.js
@@ -1,10 +1,10 @@
 const { GoogleSpreadsheet } = require("google-spreadsheet");
-const creds = require("../../secret/spreadsheet-credentials.json");
-const { id } = require("../../secret/collectionwrite.json");
-const { api_key } = require("../../secret/google-api-key.json");
+const creds = require("../secret/spreadsheet-credentials.json");
+const { id } = require("../secret/collectionwrite.json");
+const { api_key } = require("../secret/google-api-key.json");
 
-const localCollection = require("../data/collection.json");
-const { headers: headerValues } = require("../data/headers.json");
+const localCollection = require("./collection.json");
+const { headers: headerValues } = require("./headers.json");
 
 const mainFunction = async () => {
   const doc = new GoogleSpreadsheet(id);
