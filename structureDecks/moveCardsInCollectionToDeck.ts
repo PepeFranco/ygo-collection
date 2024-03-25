cb8766773bb3ce9313ef7714f03c0577f19790cd
PepeFranco <jose.franco@outlook.com>
2024-03-25 21:37:23 +1100

    move warrior's triumph

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Structure Deck: Fury from the Deep";
+const deckNameToUpdate = "Structure Deck: Warrior's Triumph";
 
 const mainFunction = async () => {
   try {
