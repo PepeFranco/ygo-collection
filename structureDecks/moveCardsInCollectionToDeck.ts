62eb1e38c7877f8641bfc8648669b7729b4a3cba
PepeFranco <jose.franco@outlook.com>
2024-03-27 21:56:23 +1100

    mofed new cards

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "";
+const deckNameToUpdate = "Structure Deck: Dragon's Roar";
 
 const mainFunction = async () => {
   try {
