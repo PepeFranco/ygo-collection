f14260b164334cb32d4e90b1c3085ee55b641fce
PepeFranco <jose.franco@outlook.com>
2024-03-27 21:38:26 +1100

    update with new cards

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Structure Deck: Warrior's Triumph";
+const deckNameToUpdate = "";
 
 const mainFunction = async () => {
   try {
