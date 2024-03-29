d824fa53aeb7ad30312940af8961c84528b9b50c
PepeFranco <jose.franco@outlook.com>
2024-03-29 22:16:48 +1100

    update

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Structure Deck: Spellcaster's Judgment";
+const deckNameToUpdate = "Structure Deck: Invincible Fortress";
 
 const mainFunction = async () => {
   try {
