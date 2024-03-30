fea335f43c0fffcc6f1b1e714209c4fbd88f6744
PepeFranco <jose.franco@outlook.com>
2024-03-30 19:59:11 +1100

    lord of the storm

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Structure Deck: Invincible Fortress";
+const deckNameToUpdate = "Structure Deck: Lord of the Storm";
 
 const mainFunction = async () => {
   try {
