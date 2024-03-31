c15b7035c3636739fd8f59026953026fa37a878c
PepeFranco <jose.franco@outlook.com>
2024-03-31 21:46:53 +1100

    Structure Deck: Machine Re-Volt

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Structure Deck: Dinosaur's Rage";
+const deckNameToUpdate = "Structure Deck: Machine Re-Volt";
 
 const mainFunction = async () => {
   try {
