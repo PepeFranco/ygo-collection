3d0466ed52f61b48ae12eb1b938c5037b7188dad
PepeFranco <jose.franco@outlook.com>
2024-04-01 23:16:57 +1100

    Rise of the Dragon Lords Structure Deck

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Structure Deck: Machine Re-Volt";
+const deckNameToUpdate = "Rise of the Dragon Lords Structure Deck";
 
 const mainFunction = async () => {
   try {
