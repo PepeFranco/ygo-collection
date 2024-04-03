0cfeeae577d714c024ecd4d0ba9a7da3a8480178
PepeFranco <jose.franco@outlook.com>
2024-04-03 20:38:04 +1100

    Zombie World Structure Deck

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "The Dark Emperor Structure Deck";
+const deckNameToUpdate = "Zombie World Structure Deck";
 
 const mainFunction = async () => {
   try {
