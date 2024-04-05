fab19874a44d02b0a7994485bb1b2cf545da35c5
PepeFranco <jose.franco@outlook.com>
2024-04-05 17:13:27 +1100

    Warriors' Strike Structure Deck

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Spellcaster's Command Structure Deck";
+const deckNameToUpdate = "Warriors' Strike Structure Deck";
 
 const mainFunction = async () => {
   try {
