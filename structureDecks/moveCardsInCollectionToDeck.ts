273fbe6ba62723db71a2e3c76ca0203e90e3dd98
PepeFranco <jose.franco@outlook.com>
2024-04-06 22:22:43 +1100

    Structure Deck: Marik (TCG)

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Machina Mayhem Structure Deck";
+const deckNameToUpdate = "Structure Deck: Marik (TCG)";
 
 const mainFunction = async () => {
   try {
