e72f611793d20e414fd75e42b9aa243983f6e921
PepeFranco <jose.franco@outlook.com>
2024-04-06 22:37:18 +1100

    Dragunity Legion Structure Deck

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Structure Deck: Marik (TCG)";
+const deckNameToUpdate = "Dragunity Legion Structure Deck";
 
 const mainFunction = async () => {
   try {
