04a710997ef80ea57ce0aee7fdb938ec9de0fb50
PepeFranco <jose.franco@outlook.com>
2024-04-06 23:15:31 +1100

    Lost Sanctuary Structure Deck

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Dragunity Legion Structure Deck";
+const deckNameToUpdate = "Lost Sanctuary Structure Deck";
 
 const mainFunction = async () => {
   try {
