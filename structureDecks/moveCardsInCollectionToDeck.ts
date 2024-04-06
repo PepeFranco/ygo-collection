4d843678cbddac35ce030f09986ccdb2b84d4a7e
PepeFranco <jose.franco@outlook.com>
2024-04-06 12:33:21 +1100

    Machina Mayhem Structure Deck

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Warriors' Strike Structure Deck";
+const deckNameToUpdate = "Machina Mayhem Structure Deck";
 
 const mainFunction = async () => {
   try {
