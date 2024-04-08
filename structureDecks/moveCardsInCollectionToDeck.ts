a4b616740e400b357f83c2d713f1e95f710452cb
PepeFranco <jose.franco@outlook.com>
2024-04-08 21:57:29 +1000

    Dragons Collide Structure Deck

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Gates of the Underworld Structure Deck";
+const deckNameToUpdate = "Dragons Collide Structure Deck";
 
 const mainFunction = async () => {
   try {
