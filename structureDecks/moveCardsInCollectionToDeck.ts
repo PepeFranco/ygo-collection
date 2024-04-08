0aea7b950586b29822b71c5495b2b097e194c663
PepeFranco <jose.franco@outlook.com>
2024-04-08 21:26:16 +1000

    Gates of the Underworld Structure Deck

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Lost Sanctuary Structure Deck";
+const deckNameToUpdate = "Gates of the Underworld Structure Deck";
 
 const mainFunction = async () => {
   try {
