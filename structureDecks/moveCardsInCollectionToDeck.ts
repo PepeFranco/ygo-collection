fd550a5d4766c19ff02251fb413434bc12a42be3
PepeFranco <jose.franco@outlook.com>
2024-04-10 22:28:33 +1000

    Saga of Blue-Eyes White Dragon Structure Deck

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Onslaught of the Fire Kings Structure Deck";
+const deckNameToUpdate = "Saga of Blue-Eyes White Dragon Structure Deck";
 
 const mainFunction = async () => {
   try {
