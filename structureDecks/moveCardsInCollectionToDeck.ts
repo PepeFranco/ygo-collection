d31b87206bc8870cadbfddd41caa2d4b7e3630bd
PepeFranco <jose.franco@outlook.com>
2024-04-11 21:23:03 +1000

    Realm of Light Structure Deck

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Cyber Dragon Revolution Structure Deck";
+const deckNameToUpdate = "Realm of Light Structure Deck";
 
 const mainFunction = async () => {
   try {
