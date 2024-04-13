2f3f60dede772840ef718f03ef7c375b49615bbd
PepeFranco <jose.franco@outlook.com>
2024-04-13 21:23:02 +1000

    Master of Pendulum Structure Deck

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Synchron Extreme Structure Deck";
+const deckNameToUpdate = "Master of Pendulum Structure Deck";
 
 const mainFunction = async () => {
   try {
