06fbcacdcde134cbf169352132d2db123c667564
PepeFranco <jose.franco@outlook.com>
2024-04-15 21:59:05 +1000

    Rise of the True Dragons Structure Deck

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Emperor of Darkness Structure Deck";
+const deckNameToUpdate = "Rise of the True Dragons Structure Deck";
 
 const mainFunction = async () => {
   try {
