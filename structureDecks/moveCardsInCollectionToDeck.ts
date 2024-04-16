6d70a95fe8c62e22d5144949f950d2c714b8d850
PepeFranco <jose.franco@outlook.com>
2024-04-16 20:52:21 +1000

    Pendulum Domination Structure Deck

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Structure Deck: Yugi Muto";
+const deckNameToUpdate = "Pendulum Domination Structure Deck";
 
 const mainFunction = async () => {
   try {
