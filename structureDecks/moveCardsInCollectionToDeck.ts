576365831bb4fde8e3bf60b571404a5fdd79d7ac
PepeFranco <jose.franco@outlook.com>
2024-04-19 20:21:10 +1000

    Structure Deck: Wave of Light

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Machine Reactor Structure Deck";
+const deckNameToUpdate = "Structure Deck: Wave of Light";
 
 const mainFunction = async () => {
   try {
