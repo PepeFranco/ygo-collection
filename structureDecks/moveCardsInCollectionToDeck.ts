81a002c8aa5098d5621600ca46435b3dce5ebe61
PepeFranco <jose.franco@outlook.com>
2024-04-19 21:27:10 +1000

    Legendary Hero Decks

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Structure Deck: Powercode Link";
+const deckNameToUpdate = "Legendary Hero Decks - Aesir";
 
 const mainFunction = async () => {
   try {
