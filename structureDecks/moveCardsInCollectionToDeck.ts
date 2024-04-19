6460f49633fb83ae9d6cf95129605f199f26a38c
PepeFranco <jose.franco@outlook.com>
2024-04-19 20:51:04 +1000

    Structure Deck: Powercode Link

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Structure Deck: Lair of Darkness";
+const deckNameToUpdate = "Structure Deck: Powercode Link";
 
 const mainFunction = async () => {
   try {
