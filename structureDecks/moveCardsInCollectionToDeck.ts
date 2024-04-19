05c9e157b7cc7b45b72bb5fe15be0f96079cd89f
PepeFranco <jose.franco@outlook.com>
2024-04-19 20:35:40 +1000

    Structure Deck: Lair of Darkness

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Structure Deck: Wave of Light";
+const deckNameToUpdate = "Structure Deck: Lair of Darkness";
 
 const mainFunction = async () => {
   try {
