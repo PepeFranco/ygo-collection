88270d30b458fa4fd52ce30fe653a3579b919587
PepeFranco <jose.franco@outlook.com>
2024-04-19 22:19:45 +1000

    Legendary Hero Decks - Phantom Knights

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Legendary Hero Decks - HERO";
+const deckNameToUpdate = "Legendary Hero Decks - Phantom Knights";
 
 const mainFunction = async () => {
   try {
