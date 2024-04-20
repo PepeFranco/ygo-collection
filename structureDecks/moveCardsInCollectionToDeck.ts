d9d8ec90213cfbf80edd947568295555e6f112be
PepeFranco <jose.franco@outlook.com>
2024-04-20 21:20:38 +1000

    Structure Deck: Zombie Horde

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Legendary Hero Decks - Phantom Knights";
+const deckNameToUpdate = "Structure Deck: Zombie Horde";
 
 const mainFunction = async () => {
   try {
