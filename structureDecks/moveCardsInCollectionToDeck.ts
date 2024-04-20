69ce0a40e094f69abd3398e67ae4e98d45b2360e
PepeFranco <jose.franco@outlook.com>
2024-04-20 22:07:31 +1000

    Structure Deck: Rokket Revolt

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Structure Deck: Order of the Spellcasters";
+const deckNameToUpdate = "Structure Deck: Rokket Revolt";
 
 const mainFunction = async () => {
   try {
