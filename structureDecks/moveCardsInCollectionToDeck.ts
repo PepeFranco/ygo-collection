289a75f06ec458b6d747c371ed7de986a0467447
PepeFranco <jose.franco@outlook.com>
2024-04-22 21:11:13 +1000

    Structure Deck: Mechanized Madness

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Structure Deck: Shaddoll Showdown";
+const deckNameToUpdate = "Structure Deck: Mechanized Madness";
 
 const mainFunction = async () => {
   try {
