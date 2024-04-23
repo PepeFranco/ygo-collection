20ac5712e4e51dd6f74fc6266a4598bdeea5925d
PepeFranco <jose.franco@outlook.com>
2024-04-23 21:16:00 +1000

    Structure Deck: Freezing Chains

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Structure Deck: Spirit Charmers";
+const deckNameToUpdate = "Structure Deck: Freezing Chains";
 
 const mainFunction = async () => {
   try {
