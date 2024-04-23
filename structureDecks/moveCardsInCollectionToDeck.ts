644da539dcbe452eba886b4a93429e9f4b7a2ffb
PepeFranco <jose.franco@outlook.com>
2024-04-23 22:06:54 +1000

    Structure Deck: Albaz Strike

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Structure Deck: Cyber Strike";
+const deckNameToUpdate = "Structure Deck: Albaz Strike";
 
 const mainFunction = async () => {
   try {
