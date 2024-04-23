99fa0b32044829fbd3d3c8b192efc9a2ad77bf29
PepeFranco <jose.franco@outlook.com>
2024-04-23 21:42:50 +1000

    Structure Deck: Cyber Strike

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Structure Deck: Freezing Chains";
+const deckNameToUpdate = "Structure Deck: Cyber Strike";
 
 const mainFunction = async () => {
   try {
