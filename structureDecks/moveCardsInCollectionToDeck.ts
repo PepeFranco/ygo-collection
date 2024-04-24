1f3927a90eff124b9b4dad7ebf72bf3a451c48d2
PepeFranco <jose.franco@outlook.com>
2024-04-24 20:34:43 +1000

    Structure Deck: Legend of the Crystal Beasts

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Structure Deck: Albaz Strike";
+const deckNameToUpdate = "Structure Deck: Legend of the Crystal Beasts";
 
 const mainFunction = async () => {
   try {
