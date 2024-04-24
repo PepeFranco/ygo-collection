6c543cc5ab00b5f417381660dacbd2b3b0c635c9
PepeFranco <jose.franco@outlook.com>
2024-04-24 21:45:04 +1000

    Structure Deck: Dark World

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Structure Deck: Legend of the Crystal Beasts";
+const deckNameToUpdate = "Structure Deck: Dark World";
 
 const mainFunction = async () => {
   try {
