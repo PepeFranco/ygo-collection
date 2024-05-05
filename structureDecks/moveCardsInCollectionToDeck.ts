f9bbe75bae059c86a19c8a042b6ef8f92ee50332
PepeFranco <jose.franco@outlook.com>
2024-05-05 20:42:05 +1000

    moved some cards

--- structureDecks/moveCardsInCollectionToDeck.ts
+++ structureDecks/moveCardsInCollectionToDeck.ts
@@ -7,7 +7,7 @@ const cardsFor3Sets: CardForXSet[] = require("./cardsFor3Sets.json");
 const collection: CollectionRow[] = require("../data/collection.json");
 const collectionCopy = [...collection];
 
-const deckNameToUpdate = "Structure Deck: Powercode Link";
+const deckNameToUpdate = "Rise of the True Dragons Structure Deck";
 
 const mainFunction = async () => {
   try {
