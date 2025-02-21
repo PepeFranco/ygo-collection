2f058fdb618c01d71490f9e7ff14e7eebae54aef
PepeFranco <jose.franco@outlook.com>
2025-02-21 22:31:54 +1100

    test

--- data/downloadCollectionFromGDrive.test.ts
+++ data/downloadCollectionFromGDrive.test.ts
@@ -3,6 +3,7 @@ import { http, HttpResponse } from "msw";
 import { setupServer } from "msw/node";
 import mockFS from "mock-fs";
 import fs from "node:fs";
+import path from "path";
 
 jest.mock("../secret/collectionread.json", () => ({
   url: "http://fake-gdrive-url",
@@ -24,7 +25,7 @@ describe("downloadCollectionFromGDrive", () => {
     const server = setupServer(...handlers);
     server.listen();
 
-    mockFS({});
+    mockFS({ data: {} });
   });
 
   afterEach(() => {
@@ -34,7 +35,130 @@ describe("downloadCollectionFromGDrive", () => {
   it("should download the collection", async () => {
     await downloadCollectionFromGDrive();
 
-    const headerFile = fs.readFileSync("./headers.json", "utf8");
-    expect(headerFile).toEqual(``);
+    const headerFile = fs.readFileSync(
+      path.join(__dirname, "headers.json"),
+      "utf8"
+    );
+    expect(headerFile).toMatchInlineSnapshot(`
+      "{
+         "headers": [
+            "Name",
+            "Code",
+            "Set",
+            "Rarity",
+            "Edition",
+            "In Deck",
+            "ID",
+            "Type",
+            "ATK",
+            "DEF",
+            "Level",
+            "Card Type",
+            "Attribute",
+            "Archetype",
+            "Scale",
+            "Link Scale",
+            "Earliest Set",
+            "Earliest Date",
+            "Is Speed Duel",
+            "Is Speed Duel Legal",
+            "Keep",
+            "Price",
+            "",
+            "",
+            ""
+         ]
+      }"
+    `);
+
+    const collectionFile = fs.readFileSync(
+      path.join(__dirname, "collection.json"),
+      "utf8"
+    );
+    expect(collectionFile).toMatchInlineSnapshot(`
+      "[
+         {
+            "Name": "7",
+            "Code": "AST-091",
+            "Set": "Ancient Sanctuary",
+            "Rarity": "Common",
+            "Edition": "1st",
+            "In Deck": "",
+            "ID": "67048711",
+            "Type": "Spell Card",
+            "ATK": "",
+            "DEF": "",
+            "Level": "",
+            "Card Type": "Continuous",
+            "Attribute": "",
+            "Archetype": "",
+            "Scale": "",
+            "Link Scale": "",
+            "Earliest Set": "Ancient Sanctuary",
+            "Earliest Date": "2004-06-01",
+            "Is Speed Duel": "No",
+            "Is Speed Duel Legal": "FALSE",
+            "Keep": "",
+            "Price": "0",
+            "field23": "",
+            "field24": "",
+            "field25": ""
+         },
+         {
+            "Name": "7",
+            "Code": "DR2-EN204",
+            "Set": "Dark Revelation Volume 2",
+            "Rarity": "Common",
+            "Edition": "",
+            "In Deck": "",
+            "ID": "67048711",
+            "Type": "Spell Card",
+            "ATK": "",
+            "DEF": "",
+            "Level": "",
+            "Card Type": "Continuous",
+            "Attribute": "",
+            "Archetype": "",
+            "Scale": "",
+            "Link Scale": "",
+            "Earliest Set": "Ancient Sanctuary",
+            "Earliest Date": "2004-06-01",
+            "Is Speed Duel": "No",
+            "Is Speed Duel Legal": "FALSE",
+            "Keep": "",
+            "Price": "3.55",
+            "field23": "",
+            "field24": "",
+            "field25": ""
+         },
+         {
+            "Name": "4-Starred Ladybug of Doom",
+            "Code": "YSYR-EN010",
+            "Set": "Starter Deck: Yugi Reloaded",
+            "Rarity": "Common",
+            "Edition": "",
+            "In Deck": "",
+            "ID": "83994646",
+            "Type": "Flip Effect Monster",
+            "ATK": "800",
+            "DEF": "1200",
+            "Level": "3",
+            "Card Type": "Insect",
+            "Attribute": "WIND",
+            "Archetype": "",
+            "Scale": "",
+            "Link Scale": "",
+            "Earliest Set": "Pharaoh's Servant",
+            "Earliest Date": "2002-10-20",
+            "Is Speed Duel": "No",
+            "Is Speed Duel Legal": "FALSE",
+            "Keep": "",
+            "Price": "1.38",
+            "field23": "",
+            "field24": "",
+            "field25": ""
+         }
+      ]"
+    `);
   });
 });
--- data/downloadCollectionFromGDrive.ts
+++ data/downloadCollectionFromGDrive.ts
@@ -2,6 +2,7 @@ import { CollectionRow } from "./data.types";
 import axios from "axios";
 import fs from "node:fs";
 import csv from "csvtojson";
+import path from "path";
 
 import collectionSecret from "../secret/collectionread.json";
 
@@ -12,35 +13,20 @@ export const downloadCollectionFromGDrive = async () => {
       console.error(e);
     });
   if (result) {
-    csv()
-      .fromString(result.data)
-      .then((collection: CollectionRow[]) => {
-        const headers = result.data
-          .split("\n")[0]
-          .split(",")
-          .map((h: any) => h.replace(/"/g, ""));
-        fs.writeFile(
-          "./data/headers.json",
-          JSON.stringify({ headers }, null, 3),
-          (err: any) => {
-            if (err) {
-              console.error(err);
-              return;
-            }
-          }
-        );
-        // console.log(`⬇️ Downloaded ${collection.length} items`);
-        // fs.writeFile(
-        //   "./data/collection.json",
-        //   JSON.stringify(collection, null, 3),
-        //   (err: any) => {
-        //     if (err) {
-        //       console.error(err);
-        //       return;
-        //     }
-        //   }
-        // );
-      });
+    const collection: CollectionRow[] = await csv().fromString(result.data);
+    const headers = result.data
+      .split("\n")[0]
+      .split(",")
+      .map((h: any) => h.replace(/"/g, ""));
+    fs.writeFileSync(
+      path.join(__dirname, "headers.json"),
+      JSON.stringify({ headers }, null, 3)
+    );
+    console.log(`⬇️ Downloaded ${collection.length} items`);
+    fs.writeFileSync(
+      "./data/collection.json",
+      JSON.stringify(collection, null, 3)
+    );
   }
 };
 
