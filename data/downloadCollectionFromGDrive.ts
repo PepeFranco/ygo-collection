1b24257b7a1119a5a069ab206457210388deb5f1
PepeFranco <jose.franco@outlook.com>
2025-02-21 22:40:19 +1100

    test and refactor

--- data/downloadCollectionFromGDrive.ts
+++ data/downloadCollectionFromGDrive.ts
@@ -1,5 +1,4 @@
 import { CollectionRow } from "./data.types";
-import axios from "axios";
 import fs from "node:fs";
 import csv from "csvtojson";
 import path from "path";
@@ -7,26 +6,29 @@ import path from "path";
 import collectionSecret from "../secret/collectionread.json";
 
 export const downloadCollectionFromGDrive = async () => {
-  const result = await axios
-    .get<string>(collectionSecret.url)
-    .catch((e: any) => {
-      console.error(e);
-    });
-  if (result) {
-    const collection: CollectionRow[] = await csv().fromString(result.data);
-    const headers = result.data
-      .split("\n")[0]
-      .split(",")
-      .map((h: any) => h.replace(/"/g, ""));
-    fs.writeFileSync(
-      path.join(__dirname, "headers.json"),
-      JSON.stringify({ headers }, null, 3)
-    );
-    console.log(`⬇️ Downloaded ${collection.length} items`);
-    fs.writeFileSync(
-      "./data/collection.json",
-      JSON.stringify(collection, null, 3)
-    );
+  try {
+    const result = await fetch(collectionSecret.url);
+    if (result.ok) {
+      const rawData = await result.text();
+      const headers = await rawData
+        .split("\n")[0]
+        .split(",")
+        .map((h: any) => h.replace(/"/g, ""));
+      fs.writeFileSync(
+        path.join(__dirname, "headers.json"),
+
+        JSON.stringify({ headers }, null, 3)
+      );
+
+      const collection: CollectionRow[] = await csv().fromString(rawData);
+      console.log(`⬇️ Downloaded ${collection.length} items`);
+      fs.writeFileSync(
+        "./data/collection.json",
+        JSON.stringify(collection, null, 3)
+      );
+    }
+  } catch (error) {
+    console.error(error);
   }
 };
 
