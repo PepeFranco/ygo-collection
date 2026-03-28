edd7e03f8b357e103e8372b8dafd7e65b5e014aa
PepeFranco <jose.franco@outlook.com>
2026-03-28 13:16:22 +1100

    keep less cards

--- retroFormats/markCardsToKeep.ts
+++ retroFormats/markCardsToKeep.ts
@@ -2,15 +2,17 @@ import collection from "../data/collection.json";
 import legalCards from "../data/speedDuel/legalCards.json";
 import path from "path";
 import fs from "fs";
-import _ from "lodash";
+import _, { add } from "lodash";
 import { CollectionRow } from "../data/data.types";
 import { debug } from "../debug";
 
+const MAX_COPIES = 6;
 const goatDate = new Date(2005, 7, 16);
 const edisonDate = new Date(2010, 3, 25);
 const hatDate = new Date(2014, 6, 8);
 
 const collectionCopy = _.sortBy([...collection], ["Code", "Rarity", "Edition"]);
+const dictionaryOfCardsKept: Record<string, number> = {};
 
 collectionCopy.map((collectionCard: CollectionRow) => {
   const [date, month, year] = collectionCard["Earliest Date"].split("/");
@@ -28,17 +30,26 @@ collectionCopy.map((collectionCard: CollectionRow) => {
   if (legalities.length > 0) {
     const legalityString = legalities.join("|");
     debug(`${collectionCard.Name} is legal in ${legalityString}`);
-    if (!collectionCard.Keep) {
+    if (
+      !collectionCard.Keep &&
+      dictionaryOfCardsKept[collectionCard.Name] <= MAX_COPIES
+    ) {
       collectionCard.Keep = legalityString;
     } else {
       debug(
-        `${collectionCard.Name} is already marked as keep for ${collectionCard.Keep}`
+        `${collectionCard.Name} is already marked as keep for ${collectionCard.Keep}`,
       );
+    }
+    if (dictionaryOfCardsKept[collectionCard.Name] === undefined) {
+      dictionaryOfCardsKept[collectionCard.Name] = 1;
+    } else {
+      dictionaryOfCardsKept[collectionCard.Name] =
+        dictionaryOfCardsKept[collectionCard.Name] + 1;
     }
   }
 });
 
 fs.writeFileSync(
   path.join(__dirname, "../data/collection.json"),
-  JSON.stringify(collectionCopy, null, 3)
+  JSON.stringify(collectionCopy, null, 3),
 );
